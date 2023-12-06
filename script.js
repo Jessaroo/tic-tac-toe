54ff7c54e536d1e1b59d41cca34174b13151ff57
Jessaroo <j_caetana@yahoo.com>
2023-12-05 16:42:19 -0800

    Setting up JavaScript file.

---
+++ script.js
@@ -0,0 +1,12 @@
+//variables
+let won;
+let lost;
+let x;
+let o;
+let turns;
+
+//const elements
+const board = document.querySelector('.game-board');
+const playAgainBtn = document.querySelector('button-play-again');
+const boardContainer = document.querySelector('.container');
+
