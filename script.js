93e5cf6e23efc49fc6f4ec4867b130a77d01484c
Jessaroo <j_caetana@yahoo.com>
2023-12-08 14:56:36 -0800

    Working on win/tie logic.

--- script.js
+++ script.js
@@ -1,12 +1,47 @@
 //variables
 let won;
 let lost;
-let x;
-let o;
-let turns;
+let currentPlayer = 'X';
+let scoreboardX = document.getElementById('scoreboard-x');
+let scoreboardO = document.getElementById('scoreboard-o');
+let turns = document.getElementById('turn');
 
 //const elements
-const board = document.querySelector('.game-board');
-const playAgainBtn = document.querySelector('button-play-again');
-const boardContainer = document.querySelector('.container');
+const squares = document.querySelectorAll('.game-square');
+const playAgainBtn = document.getElementById('button-play-again');
 
+//event listeners
+for (let i = 0; i < squares.length; i++) {
+    squares[i].addEventListener('click', handleSquareClick);
+}
+playAgainBtn.addEventListener('click', resetGame);
+
+resetGame();
+
+function handleSquareClick(event) {
+    const selectedSquare = event.target;
+    if (selectedSquare.textContent !== ''){
+        return;
+    }
+    selectedSquare.textContent = currentPlayer;
+
+    if(checkForWinner() || checkForTie()) {
+        endGame();
+    }else {
+        currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+        updateTurnDisplay();
+    }
+}
+
+function checkForWinner() {
+    if (diagonalWin || verticalWin || horizontalWin) {
+        return true;
+    }
+    return false;
+}
+
+function checkForTie() {
+    if (chosen.length >= 4 && !checkForWinner()) {
+        console.log("Tie!")
+    }
+}
